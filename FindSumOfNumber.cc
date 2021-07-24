cb35e2aa3838ca46eeb01a5b3d5ea1c8db342220
Asion Solver <asionsolver274@gmail.com>
2021-07-24 06:59:33 +0600

    Modified: FindSumOfElements.cc file

--- FindSumOfNumber.cc
+++ FindSumOfNumber.cc
@@ -1,5 +1,4 @@
-/* C++ Program to find sum of elements
-in a given array */
+/* C++ Program to find sum of elements in a given array */
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -13,17 +12,29 @@ int sum(int arr[], int n)
  
     // Iterate through all elements
     // and add them to sum
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n; i++){
     sum += arr[i];
- 
+    }
     return sum;
 }
  
 // Driver code
 int main()
 {
-    int arr[] = {12, 3, 4, 15};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int n;
+    //int arr[n]; -->Error: Segmentation fault
+    cout<<"Enter size of array: ";
+    cin>>n;
+
+    int arr[n];
+    cout<<"Enter elements in array: ";
+    for(int i = 0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    // int arr[] = {12, 3, 4, 15};
+    // int n = sizeof(arr) / sizeof(arr[0]);
+
     cout << "Sum of given array is " << sum(arr, n);
     cout<<endl;
     return 0;
